f5a13fb3ee7416171ce0f0b4dc276e7d86524919
wangbo4 <wangbo4@vipkid.com.cn>
2020-07-09 03:01:31 +0800

    学习下 链表

---
+++ src/main/java/com/wangboak/leetcode/linklist/LinkedNode.java
@@ -0,0 +1,22 @@
+package com.wangboak.leetcode.linklist;
+
+/**
+ * @program: leetcode->LinkedNode
+ * @description: 单向链表
+ * @author: wangbo4
+ * @create: 2020-07-09 02:29
+ **/
+public class LinkedNode {
+
+    public Integer val;
+
+    public LinkedNode next;
+
+    public LinkedNode(Integer val) {
+        this.val = val;
+    }
+
+    public LinkedNode() {
+    }
+
+}
--- src/main/java/com/wangboak/leetcode/linklist/ReverseLinkList.java
+++ src/main/java/com/wangboak/leetcode/linklist/ReverseLinkList.java
@@ -1,5 +1,8 @@
 package com.wangboak.leetcode.linklist;
 
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * @program: leetcode->ReverseLinkList
  * @description: 反转链表
@@ -8,14 +11,50 @@ package com.wangboak.leetcode.linklist;
  **/
 public class ReverseLinkList {
 
+    public static Integer[] arr = {1, 2, 3, 4, 5};
+    public static LinkedNode head;
 
 
+    @Before
+    public void init() {
+        head = new LinkedNode(arr[0]);
+        LinkedNode curr = head;
+        for (int i = 1; i < arr.length; i++) {
+            curr.next = new LinkedNode(arr[i]);
+            curr = curr.next;
+        }
+    }
 
+    @Test
+    public void printLink() {
+        printLink(head);
+    }
 
+    public void printLink(LinkedNode node) {
+        while (node != null) {
+            System.out.print(node.val + ", ");
+            node = node.next;
+        }
+    }
 
+    /**
+     * 链表反转。遍历法。
+     */
+    @Test
+    public void reverseLink() {
+        LinkedNode curr = head;
+        LinkedNode pre = null;
+        LinkedNode next = null;
 
-
-
-
-
+        while (true) {
+            next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+            if (curr == null) {
+                break;
+            }
+        }
+        printLink(pre);
+    }
 }
